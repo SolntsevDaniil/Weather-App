ab57770199fca9e600a1fac35ace3147a6b9806c
SolntsevDaniil <zapasnoymail1@yandex.ru>
2018-05-12 13:50:51 +0300

    Version 1,2

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 'use strict';
 class WeatherApp {
-    constructor(temp, loc, icon, humidity, wind, direction, pressure) {
+    // Constructo init
+    constructor(temp, loc, icon, humidity, wind, direction, pressure, description) {
         this.APPID = '6c000acb4c2726dc146102c3dfb3b1e3';
         this.xhr = new XMLHttpRequest();
+        this.lon = undefined;
+        this.lat = undefined;
         this.temp = temp;
         this.loc = loc;
         this.icon = icon;
@@ -10,8 +13,10 @@ class WeatherApp {
         this.wind = wind;
         this.direction = direction;
         this.pressure = pressure;
+        this.description = description;
     };
 
+    // Updating data method
     update(weather) {
         this.temp.innerHTML = weather.temp;
         this.loc.innerHTML = weather.loc;
@@ -20,47 +25,54 @@ class WeatherApp {
         this.wind.innerHTML = weather.wind;
         this.direction.innerHTML = weather.direction;
         this.pressure.innerHTML = weather.pressure;
+        this.description.innerHTML = weather.description;
     };
 
-    updateByZip(zip) {
-        let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + zip + '&APPID=' + this.APPID + '&units=metric';
+    // Get posotion so as to know the weather
+    getCurrentPosition() {
+        // If navigator is available carry out the method
+        if(navigator.geolocation) {
+            // Position
+            navigator.geolocation.getCurrentPosition((position) => {
+                this.lon = position.coords.longitude;
+                this.lat = position.coords.latitude;
+
+                this.updateByLocation(this.lat, this.lon);
+            
+            // If user declined the geolocation show the error
+            }, function(error) {
+                if(error.code == error.PERMISSION_DENIED) { alert('Can not get current position. Please, enable the Geolocation'); }
+            });
+        // If navigator is unavailable show the error
+        } else { alert('Can not get current position, something went wrong'); };
+    };
+
+    // Make a URL and send it
+    updateByLocation(lat, lon) {
+        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${this.APPID}&units=metric`;
         this.sendRequest(url);
     };
 
+    // Getting the data from API and handling it
     sendRequest(url) {
         this.xhr.open('GET', url, true);
         this.xhr.onreadystatechange = () => {
             if(this.xhr.readyState == 4 && this.xhr.status == 200) {
                 let data = JSON.parse(this.xhr.responseText);
                 let weather = {};
-                // weather.icon = data.weather.id;
                 weather.humidity = data.main.humidity;
                 weather.wind = data.wind.speed + ' meters/sec';
-                weather.direction = this.degreesToDirection(data.wind.deg);
+                weather.direction = data.wind.deg + ' degrees';
                 weather.loc = data.name;
                 weather.temp = data.main.temp;
                 weather.pressure = data.main.pressure;
+                weather.description = data.weather[0].description;
+
+                // Update the data
                 this.update(weather);
             }
         };
         this.xhr.send();
-    };
-
-    degreesToDirection(degrees) {
-        let range = 360/16,
-            low = 360 - range / 2,
-            high = (low + range) % 360,
-            i = 60,
-            angles = ['N', 'NNE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW',, 'NNW'];
-        for(i in angles) {
-            if(degrees >= low && degrees < high) {
-                return angles[i];
-
-                low = (low + range) % 360;
-                high = (high + range) % 360;
-            }
-        }
-        return 'N';
     };
 };
 
@@ -71,6 +83,7 @@ const temp = document.querySelector('.temp'),
       wind = document.querySelector('.wind'),
       direction = document.querySelector('.direction'),
       pressure = document.querySelector('.pressure'),
-      weatherapp = new WeatherApp(temp, loc, icon, humidity, wind, direction, pressure);
+      description = document.querySelector('.description'),
+      weatherapp = new WeatherApp(temp, loc, icon, humidity, wind, direction, pressure, description);
 
-weatherapp.updateByZip(109202);
+weatherapp.getCurrentPosition();
